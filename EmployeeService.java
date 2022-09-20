b090638085d4015b09b1c9230124f596aa2fd2c7
floriandyshniku <104905468+floriandyshniku@users.noreply.github.com>
2022-09-20 23:22:38 +0200

    Add files via upload

---
+++ EmployeeService.java
@@ -0,0 +1,17 @@
+package com.springboot.demo.service;
+
+import java.util.List;
+
+import com.springboot.demo.entity.Employee;
+
+public interface EmployeeService {
+
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+	
+}
